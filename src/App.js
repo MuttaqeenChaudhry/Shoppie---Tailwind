b7317a5de7e592ad049dd8b77f6215a21dd1b152
MuttaqeenChaudhry <chmuttaqeen@gmail.com>
2023-11-24 13:23:22 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import NavMenu from './components/NavMenu';
+import Shoppie from './components/Shoppie';
 
 function App() {
   const[navMenu, setnavMenu] = useState('none');
   const handleMenu = () => {
     const NavMenu = document.getElementById('NavMenu');
+    const some1 = document.getElementById('some1');
     if(navMenu === 'none')
     {
+      some1.classList.add('BgBlur')
       NavMenu.classList.remove('FadeOut')
       NavMenu.classList.remove('FadeIn')
       NavMenu.classList.add('FadeIn')
@@ -16,6 +19,7 @@ function App() {
     }
     else
     {
+      some1.classList.remove('BgBlur')
       NavMenu.classList.remove('FadeOut')
       NavMenu.classList.add('FadeOut')
       setTimeout(() => {
@@ -28,6 +32,9 @@ function App() {
     <>
     <NavBar handleMenu = {handleMenu}/>
     <NavMenu navMenu= {navMenu} handleMenu = {handleMenu}/>
+    <div id='some1'>
+    <Shoppie/>
+    </div>
     </>
   );
 }
---
+++ src/components/Shoppie.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Shoppie() {
+  return (
+    <>
+    <h1>asd</h1>
+    <h1>asd</h1>
+    <h1>asd</h1>
+    <h1>asd</h1>
+    <h1>asd</h1>
+    <h1>asd</h1>
+    <h1>asd</h1>
+    </>
+  )
+}
