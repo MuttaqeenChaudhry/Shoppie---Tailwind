9095bccfdbd41deda74b53cac9a8a527ff87e921
MuttaqeenChaudhry <chmuttaqeen@gmail.com>
2023-11-28 14:25:53 +0500

    Map JSX

--- src/components/StuffHub.js
+++ src/components/StuffHub.js
@@ -3,17 +3,70 @@ import StuffOne from './StuffOne';
 import exampleimg from '../assets/Images/P1.jpg'
 
 export default function StuffHub() {
+    const Items = [
+        {
+            id : '0',
+            title : 'Beauty steals under $25',
+            imgUrl : 'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v1._SY304_CB573698005_.jpg',
+            hrLink : 'https://www.amazon.com/s?k=beauty&rh=p_36%3A-2500&_encoding=UTF8&content-id=amzn1.sym.948ea99a-cbb3-4484-a15c-59648e21b8ad&pd_rd_r=b1093ff4-3280-4b14-a186-4d3ad1bfc3ee&pd_rd_w=JJWxk&pd_rd_wg=UUsHN&pf_rd_p=948ea99a-cbb3-4484-a15c-59648e21b8ad&pf_rd_r=QF2HW90721H6H05QRJNR&ref=pd_gw_unk',
+            hrTitle : 'See More'
+        },
+        {
+            id : '1',
+            title : 'Shop holiday gift guide',
+            imgUrl : 'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/GiftGuides/Fuji_Holiday_Gift_guide_Desktop_Card_1x_379x304_EN._SY304_CB576347904_.jpg',
+            hrLink : 'https://www.amazon.com/s?k=beauty&rh=p_36%3A-2500&_encoding=UTF8&content-id=amzn1.sym.948ea99a-cbb3-4484-a15c-59648e21b8ad&pd_rd_r=b1093ff4-3280-4b14-a186-4d3ad1bfc3ee&pd_rd_w=JJWxk&pd_rd_wg=UUsHN&pf_rd_p=948ea99a-cbb3-4484-a15c-59648e21b8ad&pf_rd_r=QF2HW90721H6H05QRJNR&ref=pd_gw_unk',
+            hrTitle : 'Read More'
+        },{
+            id : '2',
+            title : 'Easy updates for elevated spaces',
+            imgUrl : 'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v3._SY304_CB573698005_.jpg',
+            hrLink : 'https://www.amazon.com/s?k=beauty&rh=p_36%3A-2500&_encoding=UTF8&content-id=amzn1.sym.948ea99a-cbb3-4484-a15c-59648e21b8ad&pd_rd_r=b1093ff4-3280-4b14-a186-4d3ad1bfc3ee&pd_rd_w=JJWxk&pd_rd_wg=UUsHN&pf_rd_p=948ea99a-cbb3-4484-a15c-59648e21b8ad&pf_rd_r=QF2HW90721H6H05QRJNR&ref=pd_gw_unk',
+            hrTitle : 'See More'
+        },{
+            id : '3',
+            title : 'Deals in PCs',
+            imgUrl : 'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v6._SY304_CB573698005_.jpg',
+            hrLink : 'https://www.amazon.com/s?k=beauty&rh=p_36%3A-2500&_encoding=UTF8&content-id=amzn1.sym.948ea99a-cbb3-4484-a15c-59648e21b8ad&pd_rd_r=b1093ff4-3280-4b14-a186-4d3ad1bfc3ee&pd_rd_w=JJWxk&pd_rd_wg=UUsHN&pf_rd_p=948ea99a-cbb3-4484-a15c-59648e21b8ad&pf_rd_r=QF2HW90721H6H05QRJNR&ref=pd_gw_unk',
+            hrTitle : 'Shop Home Products'
+        },
+        ,{
+            id : '4',
+            title : 'Deals in PCs',
+            imgUrl : 'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v6._SY304_CB573698005_.jpg',
+            hrLink : 'https://www.amazon.com/s?k=beauty&rh=p_36%3A-2500&_encoding=UTF8&content-id=amzn1.sym.948ea99a-cbb3-4484-a15c-59648e21b8ad&pd_rd_r=b1093ff4-3280-4b14-a186-4d3ad1bfc3ee&pd_rd_w=JJWxk&pd_rd_wg=UUsHN&pf_rd_p=948ea99a-cbb3-4484-a15c-59648e21b8ad&pf_rd_r=QF2HW90721H6H05QRJNR&ref=pd_gw_unk',
+            hrTitle : 'Shop Home Products'
+        },
+        ,{
+            id : '5',
+            title : 'Deals in PCs',
+            imgUrl : 'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v6._SY304_CB573698005_.jpg',
+            hrLink : 'https://www.amazon.com/s?k=beauty&rh=p_36%3A-2500&_encoding=UTF8&content-id=amzn1.sym.948ea99a-cbb3-4484-a15c-59648e21b8ad&pd_rd_r=b1093ff4-3280-4b14-a186-4d3ad1bfc3ee&pd_rd_w=JJWxk&pd_rd_wg=UUsHN&pf_rd_p=948ea99a-cbb3-4484-a15c-59648e21b8ad&pf_rd_r=QF2HW90721H6H05QRJNR&ref=pd_gw_unk',
+            hrTitle : 'Shop Home Products'
+        },
+        ,{
+            id : '6',
+            title : 'Deals in PCs',
+            imgUrl : 'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v6._SY304_CB573698005_.jpg',
+            hrLink : 'https://www.amazon.com/s?k=beauty&rh=p_36%3A-2500&_encoding=UTF8&content-id=amzn1.sym.948ea99a-cbb3-4484-a15c-59648e21b8ad&pd_rd_r=b1093ff4-3280-4b14-a186-4d3ad1bfc3ee&pd_rd_w=JJWxk&pd_rd_wg=UUsHN&pf_rd_p=948ea99a-cbb3-4484-a15c-59648e21b8ad&pf_rd_r=QF2HW90721H6H05QRJNR&ref=pd_gw_unk',
+            hrTitle : 'Shop Home Products'
+        },
+        ,{
+            id : '7',
+            title : 'Deals in PCs',
+            imgUrl : 'https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v6._SY304_CB573698005_.jpg',
+            hrLink : 'https://www.amazon.com/s?k=beauty&rh=p_36%3A-2500&_encoding=UTF8&content-id=amzn1.sym.948ea99a-cbb3-4484-a15c-59648e21b8ad&pd_rd_r=b1093ff4-3280-4b14-a186-4d3ad1bfc3ee&pd_rd_w=JJWxk&pd_rd_wg=UUsHN&pf_rd_p=948ea99a-cbb3-4484-a15c-59648e21b8ad&pf_rd_r=QF2HW90721H6H05QRJNR&ref=pd_gw_unk',
+            hrTitle : 'Shop Home Products'
+        },
+    ]
   return (
     <>
     <div className='absolute w-full h-fit top-2/4 bg-transparent'>
         <div style={{width:'98%'}} className='bg-transparent h-fit m-auto grid grid-cols-4 grid-rows-2 gap-y-6'>
-            <StuffOne title='Beauty steals under $25' img={exampleimg}/>
-            <StuffOne title='Shop holiday gift guides'/>
-            <StuffOne title='Easy updates for elevated spaces'/>
-            <StuffOne title='Deals in PCs'/>
-            <StuffOne title='Refresh your space'/>
-            <StuffOne title='Gaming accessories'/>
-            <StuffOne title='Toys under $25'/>
+            {Items.map((Item)=>(
+                <StuffOne key={Item.id} title={Item.title} img={Item.imgUrl} hrLink={Item.hrLink} hrTitle={Item.hrTitle}/>
+
+        ))}
         </div>
     </div>
     </>
--- src/components/StuffOne.js
+++ src/components/StuffOne.js
@@ -9,7 +9,7 @@ export default function StuffOne(props) {
             style={{ height: "15%" }}
             className="flex items-center w-11/12 m-auto bg-white"
           >
-            <h3 className="font-bold text-lg">{props.title}</h3>
+            <h3 className="font-semibold text-lg">{props.title}</h3>
           </div>
           <div
             style={{ height: "70%" }}
@@ -19,9 +19,9 @@ export default function StuffOne(props) {
           </div>
           <div
             style={{ height: "15%" }}
-            className="w-11/12 m-auto bg-white"
+            className="w-11/12 m-auto bg-white flex items-center"
           >
-            3
+            <a href={props.hrLink}  className="text-cyan-700 hover:text-orange-400">{props.hrTitle}</a>
           </div>
         </div>
       </div>
